2d0ada19448f6cbb46c606bae987c7393592e6dc
Morphy Kuffour <morphy.kuffour@uconn.edu>
2022-05-14 15:46:49 -0400

    fixing error related to show_stuff()

--- lua/lookup/lookup.lua
+++ lua/lookup/lookup.lua
@@ -6,20 +6,10 @@ local function display()
 end
 
 local function lookup()
-    -- print "lookup.lookup.lookup show_stuff: hello"
     local word = vim.api.expand("<cword>")
     print(word)
 end
 
--- Returning a Lua table at the end allows fine control of the symbols that
--- will be available outside this file. By returning the table, it allows the
--- importer to decide what name to use in their own code.
---
--- Examples:
---    local ds = require('myluamodule/definestuff')
---    ds.show_stuff()
---    local definestuff = require('myluamodule/definestuff')
---    definestuff.show_stuff()
 return {
     lookup = lookup,
 }

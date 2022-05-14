9de069f398a438c8f4da500cd77a3d1402ff5f5c
Morphy Kuffour <morphy.kuffour@uconn.edu>
2022-05-14 16:37:25 -0400

    more cleanup

--- lua/lookup/lookup.lua
+++ lua/lookup/lookup.lua
@@ -1,8 +1,32 @@
+local curl = require "plenary.curl"
+
+local function lookup()
+    local word = vim.api.expand("<cword>")
+    print(word)
+end
 
 local function show_stuff()
     print "lookup.lookup.lookup show_stuff: hello"
 end
 
+-- setup function 
+local function setup(parameters)
+end
+
+
+-- Create a command, ':DoTheThing'
+-- vim.api.nvim_create_user_command(
+--     'DoTheThing',
+--     function(input)
+--         print "Something should happen here..."
+--     end,
+--     {bang = true, desc = 'a new command to do the thing'}
+-- )
+
 return {
+    setup = setup,
     show_stuff = show_stuff,
+    lookup = lookup,
 }
+
+

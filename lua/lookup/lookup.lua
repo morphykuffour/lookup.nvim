fcf360e1fb6d063874e0e1f126f5c2c5c65ff1d2
Morphy Kuffour <morphy.kuffour@uconn.edu>
2022-05-14 16:00:59 -0400

    fixing errors to test plugin

--- lua/lookup/lookup.lua
+++ lua/lookup/lookup.lua
@@ -1,15 +1,17 @@
 
-local curl = require "plenary.curl"
-
-local function display()
-  print "yes"
+local function show_stuff()
+    print "lookup.lookup.lookup show_stuff: hello"
 end
 
-local function lookup()
-    local word = vim.api.expand("<cword>")
-    print(word)
-end
-
+-- Returning a Lua table at the end allows fine control of the symbols that
+-- will be available outside this file. By returning the table, it allows the
+-- importer to decide what name to use in their own code.
+--
+-- Examples:
+--    local ds = require('myluamodule/definestuff')
+--    ds.show_stuff()
+--    local definestuff = require('myluamodule/definestuff')
+--    definestuff.show_stuff()
 return {
-    lookup = lookup,
+    show_stuff = show_stuff,
 }

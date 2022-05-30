7fee90771a236898f7f2547434999123c81d8fe0
Morphy Kuffour <morphy.kuffour@uconn.edu>
2022-05-30 01:33:50 -0400

    -- https://gist.github.com/tylerneylon/59f4bcf316be525b30ab added json/json.lua JSON libary to parse json string to a lua table for ease
    cleaned up lookup.lua
    
    TODO: output word defintion to floating window

--- lua/lookup.lua
+++ lua/lookup.lua
@@ -1,23 +1,32 @@
 local status_ok, curl = pcall(require, "plenary.curl")
-local status_ok, path = pcall(require, "plenary.path")
-local status_ok, async = pcall(require, "plenary.async")
-local Job = require("plenary.job")
--- local eq = assert.are.same
-
-local M = {}
-
 if not status_ok then
 	return
 end
 
+local json = require("json.json")
+
+local M = {}
+
 -- file_exists checks if the provided file exists and returns a boolean
--- @param file File to check
 local file_exists = function(file)
 	file = io.open(file, "rb")
 	if file then
 		file:close()
 	end
 	return file ~= nil
+end
+
+-- get all lines from a file, returns an empty
+-- list/table if the file does not exist
+function lines_from(file)
+	if not file_exists(file) then
+		return {}
+	end
+	local lines = {}
+	for line in io.lines(file) do
+		lines[#lines + 1] = line
+	end
+	return lines
 end
 
 -- read_file Reads all lines from a file and returns the content as a table
@@ -33,7 +42,7 @@ local read_file = function(file)
 	return lines
 end
 
-
+-- debugging purposes
 local function dump(o)
 	if type(o) == "table" then
 		local s = "{ "
@@ -49,45 +58,31 @@ local function dump(o)
 	end
 end
 
-M.lookup_word = function()
-	local api_key = "b5a0e125-18fa-46cf-9222-313629c38588"
-	local word = vim.fn.expand("<cword>")
-	local req_url = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/" .. word .. "?key=" .. api_key
+function readAll(file)
+	local f = assert(io.open(file, "rb"))
+	local content = f:read("*all")
+	f:close()
+	return content
+end
 
-	-- local word_def = wor_def.body works
-	curl.request({
+M.lookup_word = function()
+	local word = vim.fn.expand("<cword>")
+	local req_url = "https://api.dictionaryapi.dev/api/v2/entries/en/" .. word
+
+	local res = curl.request({
 		url = req_url,
 		method = "get",
 		accept = "application/json",
+		-- word_def => json
 		output = "/tmp/word_def",
 	})
 
-	-- ret_def = vim.fn.json_decode(word_def.body).shortdef
-	-- word_file = vim.fn.json_encode(vim.fn.join(path.readlines("/tmp/word_def"), "\n"))
-	-- print(word_file)
-	local word_table = read_file("/tmp/word_def")
-	print(dump(word_table))
+	-- remove [ ]  from begining and end TODO: try lseek version
+	local res_output = readAll("/tmp/word_def") -- print(res_output)
+	local word_def = res_output:sub(2, -2)
+
+	local word_table = json.parse(word_def) -- word_table is a nested lua table
+	print(dump(word_table["meanings"]))
 end
-
--- test
--- M.show_stuff = function()
--- 	-- print(format_json("/tmp/word_def"))
--- end
-
--- local format_json = function(file)
--- 	local output = {}
--- 	Job
--- 		:new({
--- 			command = "jq",
--- 			args = { ".", file },
--- 			on_stdout = function(_, line)
--- 				table.insert(output, line)
--- 			end,
--- 		})
--- 		:sync()
--- 	return output
--- end
-
--- Source: https://github.com/nvim-telescope/telescope-symbols.nvim/blob/f7d7c84873c95c7bd5682783dd66f84170231704/script/downloader.lua
 
 return M
